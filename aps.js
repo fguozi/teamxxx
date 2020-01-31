61bbe3352d4b0db12987da102a41595629882c26
Feng Guozi <guozi@Fengs-MacBook-Pro-2.local>
2020-01-31 09:46:39 +0800

    d

--- aps.js
+++ aps.js
@@ -1,5 +1,9 @@
-$('.toggler').live('click',function(){
-    $(this).parent().children().toggle();  //swaps the display:none between the two spans
-    $(this).parent().parent().find('.toggled_content').slideToggle();  //swap the display of the main content with slide action
-  
+$(document).ready(function(){
+    
+    $('.toggler').live('click',function(){
+      $(this).parent().children().toggle();  //swaps the display:none between the two spans
+      $(this).parent().parent().find('.toggled_content').slideToggle();  //swap the display of the main content with slide action
+
   });
+    
+});
